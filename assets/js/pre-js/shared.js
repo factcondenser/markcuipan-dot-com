5af4068d1cf0305246a6aa9c0862f9830388f7e2
factcondenser <markpan@live.com>
2017-12-28 23:38:32 -0600

    Uncache pre-js

--- assets/js/pre-js/shared.js
+++
@@ -1,9 +0,0 @@
-$("#menu-close").click(function(a) {
-  a.preventDefault();
-  $("#sidebar-wrapper").toggleClass("active")
-});
-
-$("#menu-toggle").click(function(a) {
-  a.preventDefault();
-  $("#sidebar-wrapper").toggleClass("active")
-});
